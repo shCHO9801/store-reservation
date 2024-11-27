123ced149c356da613c4fe58d4a00572f803dd5c
shCHO9801 <csh980116@naver.com>
2024-11-27 22:16:40 +0900

    test: User 엔티티 저장 및 조회 테스트 구현 - User 엔티티 저장 및 조회 테스트를 JUnit으로 작성 - Builder 패턴을 사용해 테스트 코드에서 User 객체를 생성 - Spring Data JPA의 UserRepository를 사용하여 데이터베이스 연동 테스트 진행 - @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)를 추가하여 실제 MySQL 데이터베이스를 사용하도록 설정
    ### 해결한 문제
    - @DataJpaTest가 기본적으로 임베디드 데이터베이스(H2 등)를 사용하려는 문제 발생
    - MySQL 데이터베이스와의 연결을 위해 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) 어노테이션을 추가하여 해결
    - 테스트를 실행하며 Hibernate를 통해 User 테이블이 생성되고 데이터 저장 및 조회가 정상적으로 작동 확인

---
+++ src/main/java/com/zerobase/storereservation/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.zerobase.storereservation.repository;
+
+import com.zerobase.storereservation.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
