5eae37026b4973612a0230819ed781cdc5a23c59
shCHO9801 <csh980116@naver.com>
2024-11-28 01:31:27 +0900

    feat: User API 구현 및 관련 로직 추가
    - UserController, UserService, UserDto, UserRepository 작성
    - 사용자 등록(createUser) 및 조회(getUserById) API 구현
    - 중복된 사용자 이름 확인 로직 추가
      - UserRepository.existsByUsername 메서드 활용
      - ErrorCode.USER_ALREADY_EXISTS 예외 처리 적용
    - SecurityConfig 설정 추가 (모든 요청 허용 및 CSRF 비활성화)

---
+++ src/main/java/com/zerobase/storereservation/dto/UserDto.java
@@ -0,0 +1,22 @@
+package com.zerobase.storereservation.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+public class UserDto {
+
+    @Data
+    public static class CreateRequest {
+        private String username;
+        private String password;
+        private String role;
+    }
+
+    @Data
+    @Builder
+    public static class Response{
+        private Long id;
+        private String username;
+        private String role;
+    }
+}
