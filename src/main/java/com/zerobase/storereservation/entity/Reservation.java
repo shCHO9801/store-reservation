d354dc49e43856bd055b465bea98f53e292f9962
shCHO9801 <csh980116@naver.com>
2024-11-27 23:17:56 +0900

    feat: Reservation 엔티티 설계 및 DB 연동 테스트 구현
    - Reservation 엔티티 설계 및 ManyToOne 관계 설정
    - ReservationRepository 및 테스트 코드 작성

---
+++ src/main/java/com/zerobase/storereservation/entity/Reservation.java
@@ -0,0 +1,33 @@
+package com.zerobase.storereservation.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime reserved_at;
+
+    @Column(nullable = false)
+    private String status;
+}
